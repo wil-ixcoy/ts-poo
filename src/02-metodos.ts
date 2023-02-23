185cf1911732cc5c0cd3d4d35e0d7766e581a28b
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-02-23 16:18:14 -0600

    uso de metodos en las clases

---
+++ src/02-metodos.ts
@@ -0,0 +1,30 @@
+export class myDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    return `${this.day}/${this.month}/${this.year}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    } else if (type === 'months') {
+      this.month += amount;
+    } else {
+      this.year += amount;
+    }
+  }
+}
+
+const date = new myDate(2023, 4, 3);
+
+date.add(4, 'years');
+console.log(date.printFormat());
